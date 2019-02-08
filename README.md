c0270281d6263d9cc9420882cc7527dbcb03c8e0
karla.valcarcel <karla.valcarcel@upr.edu>
2019-02-08 15:10:41 -0500

    find_meteors.py uses requests and data from NASA to find meteor landing sites

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python-4-beginners
 A demo project using Python, following instructions on an A Cloud Guru tutorial.
+
+## Running
+
+This project requires Python 3 and the 'requests' package.
+
+'python3 find_meteors.py'
---
+++ find_meteors.py
@@ -0,0 +1,35 @@
+import math
+import urllib.request
+import json
+
+def calc_dist(lat1, lon1, lat2, lon2):
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    h = math.sin( (lat2 - lat1) / 2 ) ** 2 + \
+      math.cos(lat1) * \
+      math.cos(lat2) * \
+      math.sin( (lon2 - lon1) / 2 ) ** 2
+
+    return 6372.8 * 2 * math.asin(math.sqrt(h))
+
+def get_dist(meteor):
+    return meteor.get('distance', math.inf)
+
+my_loc = (28.045043377761125, -82.41810020059349)
+
+meteor_resp = urllib.request.urlopen('https://data.nasa.gov/resource/y77d-th95.json')
+meteor_data = json.load(meteor_resp)
+
+for meteor in meteor_data:
+    if not ('reclat' in meteor and 'reclong' in meteor): continue
+    meteor['distance'] = calc_dist(float(meteor['reclat']),
+                                   float(meteor['reclong']),
+                                   my_loc[0],
+                                   my_loc[1])
+
+meteor_data.sort(key=get_dist)
+
+print(meteor_data[0:10])
